f481c6c6a08d93cc56904043f878cbb6fb8047b5
sinacodes95 <sinacodes95@gmail.com>
2022-01-20 13:41:07 +0000

    Add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import { GroupAnagrams } from "./GroupAnagrams";
 
-const groupAnagrams = new GroupAnagrams(`../../${process.argv[2]}`);
-groupAnagrams.main();
+const handleError = (error: any) => {
+  // This function would likely trigger an external system such as Sentry or a slack integration which would alert me of the error
+  // However, in this case we will just console.log the error
+  console.log('Error Handler: ', error);
+}
+
+try {
+  const groupAnagrams = new GroupAnagrams(`../../${process.argv[2]}`);
+  groupAnagrams.main();
+} catch (error) {
+  handleError(error);
+}
